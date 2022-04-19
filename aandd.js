c6db694bbf1ae9db39eb4d08c8051a8af620021a
a8120193 <a8120193@gsuite.si.aoyama.ac.jp>
2022-04-19 18:29:52 +0900

    (三島)1-7・1-8できました

--- aandd.js
+++ aandd.js
@@ -10,3 +10,19 @@ function show(array) {
     //document.getElementById('res').innerHTML = html;
     document.getElementById('res').insertAdjacentHTML('beforeend', html);
 }
+
+function createSequentialNumbers(array, n) {
+    for (var i = 1; i <= n; i++) {
+        array.push(i);
+    }
+    show(array);
+}
+
+function createRandomNumbers(array, n) {
+    for (var k = 1; k <= n; k++) {
+        var a = Math.random() * 100;
+        a = Math.floor(a);
+        array.push(a);
+    }
+    show(array);
+}
